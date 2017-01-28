3de2b87dc6eac7f230b5466eeaca0064403f78b7
ElviOsmanlliu <ElviOsmanlliu@users.noreply.github.com>
2017-01-28 22:14:10 +0100

    added new strategy "Three of a Kind"

---
+++ src/de/trialTask/strategy/ThreeOfAKindStrategy.java
@@ -0,0 +1,67 @@
+package de.trialTask.strategy;
+
+import java.util.Arrays;
+import de.trialTask.model.PokerCard;
+import de.trialTask.model.PokerHand;
+
+public class ThreeOfAKindStrategy implements IRankingStrategy {
+	
+	private PokerCard[] handOneCards;
+	private PokerCard[] handTwoCards;
+
+	@Override
+	public int rank(PokerHand handOne, PokerHand handTwo) {
+		int result = 0;
+		
+		handOneCards = handOne.getCards().clone();
+		handTwoCards = handTwo.getCards().clone();
+		
+		if (existsThreeSameValues(handOneCards) && existsThreeSameValues(handTwoCards)) {
+			result = comparePairs(handOneCards, handTwoCards);
+		}
+		
+		return result;
+	}
+	
+	private boolean existsThreeSameValues(PokerCard[] cards){
+		Arrays.sort(cards);
+		for (int i=0; i < cards.length-1; i++) {
+			if (cards[i].compareTo(cards[i+1]) == 0
+					&& cards[i].compareTo(cards[i+2]) == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private int comparePairs(PokerCard[] handOneCards, PokerCard[] handTwoCards){
+		PokerCard[] handOneSameThree = getSameCards(handOneCards);
+		PokerCard[] handTwoSameThree = getSameCards(handTwoCards);
+		
+		return new Integer(handOneSameThree[0].getValue().getNumber())
+				.compareTo(new Integer(handTwoSameThree[0].getValue().getNumber()));
+	}
+	
+	private PokerCard[] getSameCards(PokerCard[] cards){
+		PokerCard[] sameCards = new PokerCard[3];
+		
+		Arrays.sort(cards);
+		for (int i=0; i < cards.length-1; i++) {
+			if (cards[i].compareTo(cards[i+1]) == 0
+					&& cards[i].compareTo(cards[i+2]) == 0) {
+				sameCards[0] = cards[i];
+				sameCards[1] = cards[i+1];
+				sameCards[2] = cards[i+2];
+				break;
+			}
+		}
+		
+		return sameCards;
+	}
+	
+	@Override
+	public String toString() {
+		return "Three of a Kind";
+	}
+
+}
---
+++ test/test/de/trialTask/strategy/ThreeOfAKindStrategyTest.java
@@ -0,0 +1,46 @@
+package test.de.trialTask.strategy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.trialTask.model.CardSuite;
+import de.trialTask.model.CardValue;
+import de.trialTask.model.PokerCard;
+import de.trialTask.model.PokerHand;
+import de.trialTask.strategy.ThreeOfAKindStrategy;
+
+public class ThreeOfAKindStrategyTest {
+	
+	private PokerHand handOne;
+	private PokerHand handTwo;
+	private ThreeOfAKindStrategy strategy;
+
+	@Before
+	public void setUp() throws Exception {
+		handOne = new PokerHand();
+		handTwo = new PokerHand();
+		strategy = new ThreeOfAKindStrategy();
+	}
+	
+	@Test
+	public void testHandWithHighestValueOfTheThreeSameCardsWins() {
+		handOne.setCards(generatePokerCardsWithThreeSameCardValues(CardValue.QUEEN));
+		handTwo.setCards(generatePokerCardsWithThreeSameCardValues(CardValue.FIVE));
+		
+		int result = strategy.rank(handOne, handTwo);
+		assertEquals(1, result);
+	}
+	
+	private PokerCard[] generatePokerCardsWithThreeSameCardValues(CardValue sameCardValue) {
+		PokerCard four = new PokerCard(CardSuite.DIAMOND, CardValue.TWO);
+		PokerCard two = new PokerCard(CardSuite.DIAMOND, sameCardValue);
+		PokerCard three = new PokerCard(CardSuite.HEART, sameCardValue);
+		PokerCard five = new PokerCard(CardSuite.DIAMOND, CardValue.THREE);
+		PokerCard one = new PokerCard(CardSuite.CLUB, sameCardValue);
+		
+		PokerCard[] cards = {one, two, three, four, five};
+		return cards;
+	}
+}
